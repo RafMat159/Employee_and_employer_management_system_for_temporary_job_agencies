3386c9758cad24835136f0b9c14eea3163b817b5
RafMat159 <rmatusiak159@gmail.com>
2022-11-30 16:55:14 +0100

    Initial commit

---
+++ src/main/java/com/company/system_zarzadzania_dla_agencji_pracy/SystemZarzadzaniaDlaAgencjiPracyApplication.java
@@ -0,0 +1,13 @@
+package com.company.system_zarzadzania_dla_agencji_pracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemZarzadzaniaDlaAgencjiPracyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemZarzadzaniaDlaAgencjiPracyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/system_zarzadzania_dla_agencji_pracy/SystemZarzadzaniaDlaAgencjiPracyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.system_zarzadzania_dla_agencji_pracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemZarzadzaniaDlaAgencjiPracyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
