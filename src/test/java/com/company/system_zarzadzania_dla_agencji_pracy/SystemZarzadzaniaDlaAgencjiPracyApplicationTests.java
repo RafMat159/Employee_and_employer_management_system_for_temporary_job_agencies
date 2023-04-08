47882fd59bcd200b7d7e6779a246866d344a126e
Rafał Matusiak <rmatusiak159@gmail.com>
2023-04-08 12:21:16 +0200

    removed redundant dependency and added (classes = SystemZarzadzaniaDlaAgencjiPracyApplicationTests.class) to Test class

--- src/test/java/com/company/system_zarzadzania_dla_agencji_pracy/SystemZarzadzaniaDlaAgencjiPracyApplicationTests.java
+++ src/test/java/com/company/system_zarzadzania_dla_agencji_pracy/SystemZarzadzaniaDlaAgencjiPracyApplicationTests.java
@@ -3,7 +3,7 @@ package com.company.system_zarzadzania_dla_agencji_pracy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SystemZarzadzaniaDlaAgencjiPracyApplicationTests.class)
 class SystemZarzadzaniaDlaAgencjiPracyApplicationTests {
 
 	@Test
